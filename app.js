0f7f260738f9d0247e42e4b629a8b47d1a3e7f1f
Ruchit Yadav <ruchityadav955@gmail.com>
2021-12-06 11:35:23 +0530

    express rest API with mongoose part 1

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
-var promoRouter = require('./routes/promoRouter');
-var leaderRouter = require('./routes/leaderRouter');
+// var promoRouter = require('./routes/promoRouter');
+// var leaderRouter = require('./routes/leaderRouter');
+
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+  console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
+
 
 var app = express();
 
@@ -25,8 +36,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dishes',dishRouter);
-app.use('/promotions',promoRouter);
-app.use('/leaders',leaderRouter);
+// app.use('/promotions',promoRouter);
+// app.use('/leaders',leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,48 +1,79 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dishRouter = express.Router();
+const mongoose = require('mongoose');
+const Dishes = require('../models/dishes');
 
 dishRouter.use(bodyParser.json());
 
 dishRouter.route('/')
-.all((req,res,next)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    next();
-})
 .get((req,res,next)=>{
-    res.end('send all dishes to you');
+    Dishes.find({})
+        .then((dishs)=>{
+            res.statusCode = 200;
+            res.setHeader('Content-Type','application/json');
+            res.json(dishs)
+        },(err)=>next(err))
+        .catch((err)=>next(err));
 })
 .post((req,res,next)=>{
-    res.end('add all dishes to you'+req.body.name+'with details'+req.body.description);
+    Dishes.create(req.body)
+    .then((dish)=>{
+        console.log("dish is created "+dish);
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dish);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
+
 })
 .put((req,res,next)=>{
     res.statusCode = 403;
     res.end('put opration is not suported');
 })
 .delete((req,res,next)=>{
-    res.end('deleted all the dishes');
+    Dishes.remove({})
+    .then((resp)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(resp);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
 });
 
 dishRouter.route('/:dishId')
-.all((req,res,next)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    next();
-})
 .get((req,res,next)=>{
-    res.end('send details of dish'+req.params.dishId+'to you');
+    Dishes.findById(req.params.dishId)
+    .then((dish)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dish)
+    },(err)=>next(err))
+    .catch((err)=>next(err));
 })
-post((req,res,next)=>{
+.post((req,res,next)=>{
     res.statusCode = 403;
     res.end('post opration is not suported'+req.params.dishId);
 })
-put((req,res,next)=>{
-    res.write('updating the dish:'+req.params.dishId);
-    res.end('will update'+req.body.name+'with details'+req.body.description);
-});
-delete((req,res,next)=>{
-    res.end('deleting dish'+req.body.dishId);
+.put((req,res,next)=>{
+    Dishes.findByIdAndUpdate(req.params.dishId,{
+        $set : req.body
+    } , {new : true})
+    .then((dish)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(dish)
+    },(err)=>next(err))
+    .catch((err)=>next(err));
+})
+.delete((req,res,next)=>{
+    Dishes.findByIdAndRemove(req.params.dishId)
+    .then((resp)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json(resp);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
 });
 
 
