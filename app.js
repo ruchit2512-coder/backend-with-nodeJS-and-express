9cb875e57e8b2de7968b90dac164684a26363e78
Ruchit Yadav <ruchityadav955@gmail.com>
2021-12-08 12:17:52 +0530

    cookie implementation

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
-// var promoRouter = require('./routes/promoRouter');
-// var leaderRouter = require('./routes/leaderRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaderRouter');
 
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
@@ -30,14 +32,61 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+function auth(req,res,next){
+  console.log(req.signedCookies);
+
+  if(!req.signedCookies.user){
+    var authHeader = req.headers.authorization;
+  if(!authHeader){
+    var err = new Error("you are not authorized");
+
+    res.setHeader('WWW-Authenticate','Basic');
+    err.status = 401;
+    return next(err);
+  }
+
+  var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
+  var userName = auth[0];
+  var password = auth[1];
+
+  if(userName==='admin'&&password==='password'){
+    res.cookie('user','admin',{signed:true})
+    next();
+  }
+  else{
+    var err = new Error("you are not authorized");
+
+    res.setHeader('WWW-Authenticate','Basic');
+    err.status = 401;
+    return next(err);
+  }
+  }
+
+  else{
+    if(req.signedCookies.user==='admin'){
+      next()
+    }
+    else{
+      var err = new Error("you are not authorized");
+
+      err.status = 401;
+      return next(err);
+    }
+  }
+
+  
+}
+
+app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dishes',dishRouter);
-// app.use('/promotions',promoRouter);
-// app.use('/leaders',leaderRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
