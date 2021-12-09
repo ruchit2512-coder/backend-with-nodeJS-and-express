aa7cb51a99fbf1acc05d1c3a59d8b7bfb9beaed5
Ruchit Yadav <ruchityadav955@gmail.com>
2021-12-09 12:32:15 +0530

    authentication using passpord

--- models/user.js
+++ models/user.js
@@ -1,20 +1,13 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var passportlocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-    username :{
-        type : String,
-        required : true,
-        unique : true
-    },
-    password : {
-        type : String,
-        required : true
-    },
     admin : {
         type : Boolean,
         default : false
     }
 });
 
+User.plugin(passportlocalMongoose);
 module.exports = mongoose.model('User',User);
